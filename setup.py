ef52f9d8c78ebc91040c4916f8509e6a84543914
hemerson1 <emersonharry8@gmail.com>
2022-04-04 16:59:20 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2.2',
     description=
     'A Type-1 Diabetes Simulator as a Reinforcement Learning Environment in OpenAI gym or rllab (python implementation of UVa/Padova Simulator)',
-    url='https://github.com/jxx123/simglucose',
+    url='https://github.com/hemerson1/simglucose',
     author='Jinyu Xie',
     author_email='xjygr08@gmail.com',
     license='MIT',
